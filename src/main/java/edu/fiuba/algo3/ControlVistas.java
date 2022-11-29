0e472735e76e92cd8f2f1703a2f0fe1c71fee3bb
ivanrubin10 <ivanrubin10@gmail.com>
2022-11-29 18:37:22 -0300

    principio de interfaz

---
+++ src/main/java/edu/fiuba/algo3/ControlVistas.java
@@ -0,0 +1,32 @@
+package edu.fiuba.algo3;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ControlVistas {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void cambiarAVistaInicial(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("../../../../resources/VistaInicio.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void cambiarAVistaJugador(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("../../../../resources/VistaJugador.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
